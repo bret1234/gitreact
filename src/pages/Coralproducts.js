19a0c8327c41f2034a4d78303ddd8d9de3cb438a
bret <bret1234myevolv@hotmail.com>
2021-04-06 11:41:36 -0500

    api and pages
    added pages and api

---
+++ src/pages/Coralproducts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Coralproducts() {
+    return (
+        <div>
+            <h1>Coral Products</h1>
+        </div>
+    )
+}
+
+export default Coralproducts
---
+++ src/pages/Fishproducts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Fishproducts() {
+    return (
+        <div>
+            <h1>fish product</h1>
+        </div>
+    )
+}
+
+export default Fishproducts
