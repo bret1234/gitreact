7d5040171455ad53fe1453e1213bdccb2cbb7ab9
bret <bret1234myevolv@hotmail.com>
2021-04-06 11:44:48 -0500

    api
    added api components

---
+++ src/API/CoralAPI.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CoralAPI() {
+    return (
+        <div>
+            <h1>coral api</h1>
+        </div>
+    )
+}
+
+export default CoralAPI
---
+++ src/API/FishAPI.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FishAPI() {
+    return (
+        <div>
+            <h1>fish api</h1>
+        </div>
+    )
+}
+
+export default FishAPI
